3238a39145e31cab8a6f875d0c7827887cb723d2
fzahmed <dhaka.faisal@gmail.com>
2021-11-21 10:11:41 -0600

    made change

--- src/main/java/variousConcept/GitTest2.java
+++ src/main/java/variousConcept/GitTest2.java
@@ -4,11 +4,12 @@ public class GitTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+System.out.println("gittest1");
 System.out.println("gittest2");
-System.out.println("gittest2");
-System.out.println("gittest2");
-System.out.println("gittest2");
-System.out.println("gittest2");
+System.out.println("gittest3");
+System.out.println("gittest4");
+System.out.println("gittest5");
+System.out.println("gittest6");
 	}
 
 }
